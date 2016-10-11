62fb49e256d744aae3ca01ff5033441c3567eb08
Sam <sljohnson32@gmail.com>
2016-10-11 10:52:29 -0600

    Bug fixes

--- scripts/numberguesser.js
+++ scripts/numberguesser.js
@@ -1,42 +1,53 @@
 var guessButton = document.querySelector('.guess-button');
 var clearButton = document.querySelector('.clear-button');
-var resetButton = document.querySelector('.reset-button')
+var resetButton = document.querySelector('.reset-button');
+
+//set random number to variable...
+var secretNumber = setRandomNumber();
+
+//create random number
+function setRandomNumber() {
+  return Math.floor((Math.random() * 100) + 1);
+}
 
 //create Guess button
 guessButton.addEventListener('click', function() {
 
-//assign value for the currentGuess
-  var curretValue = (document.querySelector('#current-guess').value;
+console.log(secretNumber);
+  //assign value for the currentGuess
+  var currentValue = document.querySelector('#current-guess').value;
   var currentGuess = parseInt(currentValue, 10);
-
-  //create random number
-  function setRandomNumber() {
-    return Math.floor((Math.random() * 100) + 1);
+  
+  //if statments to confirm currentGuess is between 1 and 100
+  if (isNaN(currentGuess) === true) {
+    alert("Please enter a number between 1 and 100.");
   }
 
-  //set random number to variable...
-  var secretNumber = setRandomNumber();
+    //action to compare currentGuess to SecretNumber
+    else if (currentGuess > secretNumber) {
+      document.querySelector('.last-guess').innerText = currentGuess;
+      document.querySelector('.hint').innerText = 'That is too high!';
+    }
 
-  console.log(currentGuess);
+      else if (currentGuess < secretNumber) {
+        document.querySelector('.last-guess').innerText = currentGuess;
+        document.querySelector('.hint').innerText = 'That is too low!';
+      }
 
-//action to confirm currentGuess is between 1 and 100
-if (isNaN(currentGuess) === true) {
-  alert("Please enter a number between 1 and 100.");
-}
+        else if (currentGuess === secretNumber) {
+          document.querySelector('.last-guess').innerText = currentGuess;
+          document.querySelector('.hint').innerText = 'YOU GOT IT!';
+        }
+  });
 
-//action to compare currentGuess to SecretNumber
-else if (currentGuess > secretNumber) {
-    document.querySelector('.lastGuess').innerText = '#currentGuess';
-    document.querySelector('.hintText').innerText = 'That is too high!';
+  function checkGuess() {
+
   }
 
-else if (currentGuess < secretNumber) {
-    document.querySelector('.lastGuess').innerText = '#currentGuess';
-    document.querySelector('.hintText').innerText = 'That is too low!';
-  }
+clearButton.addEventListener('click', function() {
+    // code for clear actions
+  });
 
-else if (currentGuess === secretNumber) {
-  document.querySelector('.lastGuess').innerText = '#currentGuess';
-  document.querySelector('.hintText').innerText = 'YOU GOT IT!';
-}
-});
+resetButton.addEventListener('click', function() {
+    // code for reset actions
+  });
